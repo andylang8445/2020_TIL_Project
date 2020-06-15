24841c8bb5e166b1556d139cb1e25c90a7ae5045
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2020-06-15 01:20:26 -0400

    Started the project

---
+++ Sudoku Solving Project/Sudoku Solving Project/Sudoku Solving Project/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Sudoku Solving Project
+//
+//  Created by Hongjun Yun on 2020-06-15.
+//  Copyright © 2020 Hongjun Yun. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
